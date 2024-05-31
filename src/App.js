2ed2c29ad39d12972df28907831a618ccc1c44d6
PT <pt@MacBook-Air-cua-PT.local>
2024-06-01 03:40:06 +0700

    update 2h

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import ProductDetail from './pages/ProductDetail';
 import Cart from './components/Cart';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
     <Router>
       <div>
         <Header />
+        <SpeedInsights />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/product/:id" element={<ProductDetail />} />
