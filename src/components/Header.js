0ce69fb42e859d4e8115adcd3338fa5ab7e4848f
PT <pt@MacBook-Air-cua-PT.local>
2024-05-31 17:24:16 +0700

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
   return (
     <header>
       <h1>My E-Commerce Store</h1>
+      <p> Chinh Minh Hùng là con tó </p>
       <nav>
         <Link to="/">Home</Link>
         <Link to="/cart">Cart</Link>
